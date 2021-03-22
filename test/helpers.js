927a8ffb46f976e82268c7861ec80076da610310
reevesalic <reevesalic@gmail.com>
2021-03-22 13:25:27 -0400

    commit tree 5d7172102c6c986dea27b3bf0242c23c0255abd3

--- test/helpers.js
+++ test/helpers.js
@@ -9,10 +9,11 @@ global.window = document.defaultView;
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
     exposedProperties.push(property);
-    global[property] = document.defaultView[property];
+    global[property] = document.defaultView[property]; 
+    
   }
 });
 
 global.navigator = {
-  userAgent: 'node.js'
+  userAgent: 'node.js' 
 };
--- test/index-test.js
+++ test/index-test.js
@@ -46,12 +46,12 @@ describe('<EyesOnMe />', () => {
   it('should trigger console output when focusing', () => {
     wrapper.find('button').simulate('focus');
     expect(spy.calledOnce, 'The console output is not being triggered when focusing the button').to.be.true;
-    expect(spy.firstCall.args[0]).to.equal('Good!', 'The console output is not the expected string.');
+    expect(spy.firstCall.args[0]).to.equal('Good!', 'The console output is not the expected string.'); 
   });
 
   it('should trigger console output when removing focus (blur)', () => {
     wrapper.find('button').simulate('blur');
     expect(spy.calledOnce, 'The console output is not being triggered when focusing the button').to.be.true;
-    expect(spy.firstCall.args[0]).to.equal('Hey! Eyes on me!', 'The console output is not the expected string.'); --exit
+    expect(spy.firstCall.args[0]).to.equal('Hey! Eyes on me!', 'The console output is not the expected string.'); 
   });
 });
